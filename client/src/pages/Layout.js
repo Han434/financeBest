59977a5c8da3829848e9ef6e211c778e9f7c37ab
Han434 <112228783+Han434@users.noreply.github.com>
2024-07-03 10:54:39 +0630

    Add footer

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -3,33 +3,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Layout = () => {
   return (
+    <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./dashboard">Dashboard</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./blogs">Blogs</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./contact">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./user">LogIn</a>
-            </li>
-          </ul>
-        </div>
+      <a className="navbar-brand text-center" href="/">FinanceBest</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+       <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+         <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active text-center" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active text-center" to="/dashboard">Dashboard</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active text-center" to="/blogs">Blogs</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active text-center" to="/contact">Contact</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active text-center" to="/user">User</Link>
+        </li>
+      </ul>
       </div>
     </nav>
+
+    <Outlet />
+  </>
   )
 };
 
